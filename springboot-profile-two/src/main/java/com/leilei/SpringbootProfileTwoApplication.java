c5780c6e91c4ff0e6d58da0621a6c04a9d3d83c5
Leilei <248721866@qq.com>
2020-02-20 16:40:51 +0800

    项目启动时指定加载配置 方式二（ 开发环境 测试环境 生产环境）

---
+++ springboot-profile-two/src/main/java/com/leilei/SpringbootProfileTwoApplication.java
@@ -0,0 +1,13 @@
+package com.leilei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProfileTwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootProfileTwoApplication.class, args);
+    }
+
+}
--- springboot-redis/READEME.md
+++ springboot-redis/READEME.md
@@ -1,3 +1,5 @@
-SpringBoot整合Redis Cluster 集群 以及Redis存储结构 String Hash Set List 的使用
-
-redis的String  hash  set list 存储结构的使用在测试类中
+SpringBoot 多环境启动篇  
+编译打包其对应Yml  
+打包命令 idea 进入项目目录下 使用Terminal终端   mvn clean install -P test  此命令就会打包test环境
+打包后 在target classes 下可查看打包环境
+启动时直接java -jar 执行就ok
