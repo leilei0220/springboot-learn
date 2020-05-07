d05f8a680dd882942a6fc11b4514e68d3dc5b6be
leilei <248721866@qq.com>
2020-05-07 19:33:55 +0800

    set存储 多个对象

--- springboot-redis-cluster/src/test/java/com/leilei/springbootredis/SpringBootredisTest.java
+++ springboot-redis-cluster/src/test/java/com/leilei/springbootredis/SpringBootredisTest.java
@@ -33,104 +33,120 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 public class SpringBootredisTest {
 
-    @Autowired
-    private RedisTemplate redisTemplate;
-    @Autowired
-    private RedisUtil redisUtil;
+  @Autowired
+  private RedisTemplate redisTemplate;
+  @Autowired
+  private RedisUtil redisUtil;
 
-    /**
-     * String
-     */
-    @Test
-    public void testString() {
-        redisUtil.set("a", 11111);
-        System.out.println(redisUtil.get("a"));
-        redisUtil.set("过期时间测试", 123, 50);
-        redisUtil.del("a");
+  /**
+   * String
+   */
+  @Test
+  public void testString() {
+    redisUtil.set("a", 11111);
+    System.out.println(redisUtil.get("a"));
+    redisUtil.set("过期时间测试", 123, 50);
+    redisUtil.del("a");
+  }
+
+  /**
+   * hash
+   */
+  @Test
+  public void testMap() {
+    HashMap<String, Object> objectObjectHashMap = new HashMap<>();
+    objectObjectHashMap.put("aaa", 1);
+    objectObjectHashMap.put("aaa", "aaa");
+    objectObjectHashMap.put("a", 1);
+    objectObjectHashMap.put("qweqwr", 1L);
+    redisUtil.hmset("Map测试", objectObjectHashMap);
+
+    Map<Object, Object> map = redisUtil.hmget("Map测试");
+    for (Map.Entry<Object, Object> objectObjectEntry : map.entrySet()) {
+      System.out.println(objectObjectEntry.getKey() + "-------" + objectObjectEntry.getValue());
     }
 
-    /**
-     * hash
-     */
-    @Test
-    public void testMap() {
-        HashMap<String, Object> objectObjectHashMap = new HashMap<>();
-        objectObjectHashMap.put("aaa", 1);
-        objectObjectHashMap.put("aaa", "aaa");
-        objectObjectHashMap.put("a", 1);
-        objectObjectHashMap.put("qweqwr", 1L);
-        redisUtil.hmset("Map测试", objectObjectHashMap);
+    Object hget = redisUtil.hget("Map测试", "a");
+    System.out.println(hget);
 
-        Map<Object, Object> map = redisUtil.hmget("Map测试");
-        for (Map.Entry<Object, Object> objectObjectEntry : map.entrySet()) {
-            System.out.println(objectObjectEntry.getKey() + "-------" + objectObjectEntry.getValue());
-        }
+    redisUtil.hdel("Map测试", "qweqwr");
+  }
 
-        Object hget = redisUtil.hget("Map测试", "a");
-        System.out.println(hget);
+  /**
+   * hash
+   */
+  @Test
+  public void testMapSuper() {
+    HashMap<String, Object> map = new HashMap<>();
+    map.put("a", 1L);
+    map.put("b", 2D);
+    map.put("c", 123);
+    map.put("d", LocalDateTime.now());
+    redisUtil.hmset("leilei:1:map", map);
+    redisUtil.hmset("leilei:2:map", map);
+    redisUtil.hmset("leilei:3:map", map);
+    redisUtil.hmset("leilei:4:map", map);
+  }
 
-        redisUtil.hdel("Map测试", "qweqwr");
-    }
+  @Test
+  public void testMap2Bean() {
+    Map<String, Object> map = new HashMap<>();
+    map.put("username", "王五");
+    map.put("password", "123");
+    map.put("age", 11);
+    redisUtil.hmset("Map2Bean", map);
+    Map<Object, Object> map2Bean = redisUtil.hmget("Map2Bean");
+    User user = mapToBean(map2Bean, new User());
+    System.out.println("Map2Bean" + user);
+  }
 
-    /**
-     * hash
-     */
-    @Test
-    public void testMapSuper() {
-        HashMap<String, Object> map = new HashMap<>();
-        map.put("a", 1L);
-        map.put("b", 2D);
-        map.put("c", 123);
-        map.put("d", LocalDateTime.now());
-        redisUtil.hmset("leilei:1:map", map);
-        redisUtil.hmset("leilei:2:map", map);
-        redisUtil.hmset("leilei:3:map", map);
-        redisUtil.hmset("leilei:4:map", map);
-    }
-    @Test
-    public void testMap2Bean (){
-        Map<String, Object> map = new HashMap<>();
-        map.put("username","王五");
-        map.put("password","123");
-        map.put("age",11);
-        redisUtil.hmset("Map2Bean", map);
-        Map<Object, Object> map2Bean = redisUtil.hmget("Map2Bean");
-        User user = mapToBean(map2Bean, new User());
-        System.out.println("Map2Bean"+user);
-    }
-    /**
-     * map转对象
-     *
-     * @param map
-     * @param bean
-     * @param <T>
-     * @return
-     */
-    public static <T> T mapToBean(Map<Object, Object> map, T bean) {
-        BeanMap beanMap = BeanMap.create(bean);
-        beanMap.putAll(map);
-        return bean;
-    }
+  /**
+   * map转对象
+   *
+   * @param map
+   * @param bean
+   * @param <T>
+   * @return
+   */
+  public static <T> T mapToBean(Map<Object, Object> map, T bean) {
+    BeanMap beanMap = BeanMap.create(bean);
+    beanMap.putAll(map);
+    return bean;
+  }
 //-----------------set----------------
-    /**
-     * set
-     */
-    @Test
-    public void testSet() {
-        redisUtil.sSet("Set集合", 3, 1, 2, 5, 12, 666);
-    }
-    /**
-     * test
-     */
-    @Test
-    public void testSet1() {
-      long size = redisUtil.sGetSetSize("Set集合");
-      System.out.println(size);
-    }
 
+  /**
+   * set
+   */
+  @Test
+  public void testSet() {
+    redisUtil.sSet("Set集合", 3,3,12, 1, 2, 5, 12, 666);
+  }
 
+  /**
+   * test
+   */
+  @Test
+  public void testSetBean() {
 
-    //------------zSet-----------------
+    long l = redisUtil.sSet("魏将",
+        new User("许褚", "qq", 55),
+        new User("张辽", "dd", 62),
+        new User("典韦", "123", 49));
+    System.out.println(l);
+  }
+
+  /**
+   * test
+   */
+  @Test
+  public void testSet1() {
+    long size = redisUtil.sGetSetSize("Set集合");
+    System.out.println(size);
+  }
+
+  //------------zSet-----------------
+
   /**
    * test 添加
    */
@@ -140,6 +156,7 @@ public class SpringBootredisTest {
     redisUtil.sZset("天梯排行榜", "逍遥子", 1);
     redisUtil.sZset("天梯排行榜", "无崖子", 3);
   }
+
   /**
    * test zset 批量添加
    */
@@ -162,6 +179,7 @@ public class SpringBootredisTest {
     Long aLong = redisUtil.zRank("天梯排行榜", "无崖子");
     System.out.println(aLong);
   }
+
   /**
    * test
    */
@@ -169,6 +187,7 @@ public class SpringBootredisTest {
   public void test3() {
     redisUtil.zIncrScore("天梯排行榜", "无崖子", -0.5);
   }
+
   /**
    * test 删除 可填入多个value
    */
@@ -177,6 +196,7 @@ public class SpringBootredisTest {
     Long aLong = redisUtil.zRemove("天梯排行榜", "无崖子");
     System.out.println(aLong);
   }
+
   /**
    * test 获取score值
    */
@@ -185,6 +205,7 @@ public class SpringBootredisTest {
     Double aDouble = redisUtil.zScore("天梯排行榜", "逍遥子");
     System.out.println(aDouble);
   }
+
   /**
    * test 获取zset集合大小
    */
@@ -193,6 +214,7 @@ public class SpringBootredisTest {
     Long size = redisUtil.zSize("天梯排行榜");
     System.out.println(size);
   }
+
   /**
    * test 获取集合中的数据 正序倒序 获取指定范围数据
    */
@@ -200,11 +222,11 @@ public class SpringBootredisTest {
   public void test7() {
     //正序
     Set<Object> set = redisUtil.zRange("天梯排行榜", 0, -1);
-    set.forEach(e-> System.out.println(e));
+    set.forEach(e -> System.out.println(e));
     System.out.println("---------");
     //倒序
     Set<Object> set1 = redisUtil.reverseRange("天梯排行榜", 0, -1);
-    set1.forEach(e-> System.out.println(e));
+    set1.forEach(e -> System.out.println(e));
   }
 
   /**
@@ -217,6 +239,7 @@ public class SpringBootredisTest {
       System.out.println(objectTypedTuple.getValue() + " == " + objectTypedTuple.getScore());
     }
   }
+
   /**
    * test 根据分数最大最小值获取set集合后再根据 索引以及显示个数返回全新set集合
    */
@@ -232,99 +255,98 @@ public class SpringBootredisTest {
   }
 
 
+  /**
+   * fastjson 部分使用
+   */
+  @Test
+  public void testFastJson() {
+    //String对象转Json字符串
+    String str = "a=1&b=2&c=e";
 
+    str = str.replace("=", "\":\"");
+    System.out.println(str);
+    str = str.replace("&", "\",\"");
+    System.out.println(str);
+    str = "{\"" + str + "\"}";
+    System.out.println(str);
+    System.out.println(JSON.parseObject(str).toJSONString());
 
+    //Java对象转json字符串
+    User aa = new User("aa", "123456", 15);
+    System.out.println(aa); //User(username=aa, password=123456, age=15)
+    String s = JSON.toJSONString(aa);
+    System.out.println(s);   //{"age":15,"password":"123456","username":"aa"}
 
+    //json字符串转java对象
+    String ss = "{\"age\":15,\"password\":\"123456\",\"username\":\"aa\"}";
+    User user = JSON.parseObject(ss, User.class);
+    System.out.println(user); //User(username=aa, password=123456, age=15)
 
-    /**
-     * fastjson 部分使用
-     */
-    @Test
-    public void testFastJson (){
-        //String对象转Json字符串
-        String str = "a=1&b=2&c=e";
+    //Java集合转json字符串
+    List<Serializable> serializables = Arrays.asList(new User("a", "a", 1), new User("b", "b", 2));
+    String jsonString = JSON.toJSONString(serializables);
+    System.out.println(
+        jsonString); //[{"age":1,"password":"a","username":"a"},{"age":2,"password":"b","username":"b"}]
 
-        str = str.replace("=","\":\"");
-        System.out.println(str);
-        str = str.replace("&","\",\"");
-        System.out.println(str);
-        str = "{\"" + str +"\"}";
-        System.out.println(str);
-        System.out.println(JSON.parseObject(str).toJSONString());
+    //json字符串转java集合
+    String jsonlistString = "[{\"age\":1,\"password\":\"a\",\"username\":\"a\"},{\"age\":2,\"password\":\"b\",\"username\":\"b\"}]";
+    List<User> users = JSON.parseArray(jsonlistString, User.class);
+    users.forEach(e -> System.out
+        .println(e));  //User(username=a, password=a, age=1) User(username=b, password=b, age=2)
 
-        //Java对象转json字符串
-        User aa = new User("aa", "123456", 15);
-        System.out.println(aa); //User(username=aa, password=123456, age=15)
-        String s = JSON.toJSONString(aa);
-        System.out.println(s);   //{"age":15,"password":"123456","username":"aa"}
+    //map转Json字符串
+    HashMap<String, Object> objectObjectHashMap = new HashMap<>();
+    objectObjectHashMap.put("a", 1);
+    objectObjectHashMap.put("b", 2L);
+    objectObjectHashMap.put("c", 3D);
+    objectObjectHashMap.put("d", 3f);
+    objectObjectHashMap.put("f", LocalDateTime.now());
+    String mapjsonString = JSON.toJSONString(objectObjectHashMap);
+    System.out
+        .println(mapjsonString);  //{"a":1,"b":2,"c":3.0,"d":3.0,"f":"2020-02-18T16:40:01.203"}
 
-        //json字符串转java对象
-        String ss = "{\"age\":15,\"password\":\"123456\",\"username\":\"aa\"}";
-        User user = JSON.parseObject(ss, User.class);
-        System.out.println(user); //User(username=aa, password=123456, age=15)
-
-        //Java集合转json字符串
-        List<Serializable> serializables = Arrays.asList(new User("a", "a", 1), new User("b", "b", 2));
-        String jsonString = JSON.toJSONString(serializables);
-        System.out.println(jsonString); //[{"age":1,"password":"a","username":"a"},{"age":2,"password":"b","username":"b"}]
-
-        //json字符串转java集合
-        String jsonlistString = "[{\"age\":1,\"password\":\"a\",\"username\":\"a\"},{\"age\":2,\"password\":\"b\",\"username\":\"b\"}]";
-        List<User> users = JSON.parseArray(jsonlistString, User.class);
-        users.forEach(e-> System.out.println(e));  //User(username=a, password=a, age=1) User(username=b, password=b, age=2)
-
-        //map转Json字符串
-        HashMap<String, Object> objectObjectHashMap = new HashMap<>();
-        objectObjectHashMap.put("a", 1);
-        objectObjectHashMap.put("b", 2L);
-        objectObjectHashMap.put("c", 3D);
-        objectObjectHashMap.put("d", 3f);
-        objectObjectHashMap.put("f", LocalDateTime.now());
-        String mapjsonString = JSON.toJSONString(objectObjectHashMap);
-        System.out.println(mapjsonString);  //{"a":1,"b":2,"c":3.0,"d":3.0,"f":"2020-02-18T16:40:01.203"}
-
-        //map转json对象
-        JSONObject mapjsonobj = new JSONObject(objectObjectHashMap);
-        System.out.println("map转json对象"+mapjsonobj);
-        //json字符串转Map
-        //需要将json字符串转为json对象   json对象再转为map
-        JSONObject  jsonObject = JSONObject.parseObject(mapjsonString);
-        Map<String,Object> map = (Map<String,Object>)jsonObject;
-        for (Map.Entry<String, Object> stringObjectEntry : map.entrySet()) {
-            System.out.println(stringObjectEntry.getKey() + "-------" + stringObjectEntry.getValue());
-            //a-------1
-            //b-------2
-            //c-------3.0
-            //d-------3.0
-            //f-------2020-02-18T16:45:37.452
-        }
-
-
+    //map转json对象
+    JSONObject mapjsonobj = new JSONObject(objectObjectHashMap);
+    System.out.println("map转json对象" + mapjsonobj);
+    //json字符串转Map
+    //需要将json字符串转为json对象   json对象再转为map
+    JSONObject jsonObject = JSONObject.parseObject(mapjsonString);
+    Map<String, Object> map = (Map<String, Object>) jsonObject;
+    for (Map.Entry<String, Object> stringObjectEntry : map.entrySet()) {
+      System.out.println(stringObjectEntry.getKey() + "-------" + stringObjectEntry.getValue());
+      //a-------1
+      //b-------2
+      //c-------3.0
+      //d-------3.0
+      //f-------2020-02-18T16:45:37.452
     }
 
-    /**
-     * list
-     */
-    @Test
-    public void testList() {
-        redisUtil.lSet("LIst测试", 1);
-        redisUtil.lSet("LIst测试", 2);
-        redisUtil.lSet("LIst测试", "aaaa");
-        List<Object> list = redisUtil.lGet("LIst测试", 1, 2);
-        System.out.println(list);
-        System.out.println(redisUtil.lGetListSize("LIst测试"));
-        System.out.println(redisUtil.lGetIndex("LIst测试", 2));
-        List<Object> listo = new ArrayList<>();
-        listo.add(1);
-        listo.add(2);
-        User user = new User();
-        user.setUsername("aa");
-        user.setPassword("123");
-        user.setAge(12);
-        listo.add(JSON.toJSONString(user));
-        redisUtil.lSet("list与json测试", listo);
-        String s = (String) redisUtil.lGetIndex("list与json测试", 2);
-        User user1 = JSON.parseObject(s, User.class);
-        System.out.println(user1);
-    }
+
+  }
+
+  /**
+   * list
+   */
+  @Test
+  public void testList() {
+    redisUtil.lSet("LIst测试", 1);
+    redisUtil.lSet("LIst测试", 2);
+    redisUtil.lSet("LIst测试", "aaaa");
+    List<Object> list = redisUtil.lGet("LIst测试", 1, 2);
+    System.out.println(list);
+    System.out.println(redisUtil.lGetListSize("LIst测试"));
+    System.out.println(redisUtil.lGetIndex("LIst测试", 2));
+    List<Object> listo = new ArrayList<>();
+    listo.add(1);
+    listo.add(2);
+    User user = new User();
+    user.setUsername("aa");
+    user.setPassword("123");
+    user.setAge(12);
+    listo.add(JSON.toJSONString(user));
+    redisUtil.lSet("list与json测试", listo);
+    String s = (String) redisUtil.lGetIndex("list与json测试", 2);
+    User user1 = JSON.parseObject(s, User.class);
+    System.out.println(user1);
+  }
 }
