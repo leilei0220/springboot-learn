181b07c489da3d53838c5b6153cad5aba765ebe2
leilei <248721866@qq.com>
2020-05-07 11:54:03 +0800

    springboot-redis-cluster集群模式使用 以及五种存储结构的使用

--- springboot-redis-cluster/READEME.md
+++ springboot-redis-cluster/READEME.md
@@ -1,4 +1,4 @@
 ##springboot-redis
 SPringboot整合Redis Cluster  
 String  set  hash list 存储结构的使用
-fastjson 的使用
+并结合 fastjson 的使用
--- springboot-redis-cluster/src/main/java/com/leilei/SpringbootRedisApplication.java
+++ springboot-redis-cluster/src/main/java/com/leilei/SpringbootRedisApplication.java
@@ -3,6 +3,10 @@ package com.leilei;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author leilei
+ * @Desc springboot项目中 整合redis-cluster 集群模式的使用
+ */
 @SpringBootApplication
 public class SpringbootRedisApplication {
 
