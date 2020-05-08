6ad43f4b4f77741d3fe572704489b1435e6e676a
leilei <248721866@qq.com>
2020-05-08 19:35:28 +0800

    springboot使用拦截器 并自定义 注解 实现接口拦截与放行效果 model创建

---
+++ springboot-interceptor/src/main/java/com/leilei/SpringbootInterceptorApplication.java
@@ -0,0 +1,13 @@
+package com.leilei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootInterceptorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootInterceptorApplication.class, args);
+  }
+
+}
