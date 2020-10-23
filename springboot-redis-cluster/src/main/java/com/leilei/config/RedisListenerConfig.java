e494f71a63f8d7fe283359056d4045979dfc2d68
leipeng <leileideve@163.com>
2020-10-23 11:34:05 +0800

    redis 添加key过期监听

---
+++ springboot-redis-cluster/src/main/java/com/leilei/config/RedisListenerConfig.java
@@ -0,0 +1,23 @@
+package com.leilei.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+
+/**
+ * @author lei
+ * @version 1.0
+ * @date 2020-10-23 11:31
+ * @desc
+ */
+@Configuration
+public class RedisListenerConfig {
+    @Bean
+    RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory) {
+
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        return container;
+    }
+}
