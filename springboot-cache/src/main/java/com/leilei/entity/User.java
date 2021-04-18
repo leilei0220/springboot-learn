6849464b9f659038d0ac568c72648d72d34c2797
leilei <leileideve@163.com>
2021-04-18 16:51:46 +0800

    【新增】 sharding-cache 内存缓存使用

---
+++ springboot-cache/src/main/java/com/leilei/entity/User.java
@@ -0,0 +1,28 @@
+package com.leilei.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+/**
+ * @author lei
+ * @version 1.0
+ * @date 2021/4/18 14:57
+ * @desc
+ */
+@TableName("user")
+@Data
+public class User {
+    @TableId(type = IdType.AUTO)
+    private Integer id;
+    private String name;
+
+    private Integer age;
+
+    private String className;
+
+    private String schoolName;
+
+    private String createId;
+}
