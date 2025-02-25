8a0e152b70e975e74240fc67971a5a56194dedcc
leipeng <leileideve@163.com>
2025-02-25 17:09:02 +0800

    springboot-deepseek

---
+++ springboot-deepseek/src/main/java/com/leilei/DeepSeekController.java
@@ -0,0 +1,34 @@
+package com.leilei;
+
+import io.github.pigmesh.ai.deepseek.core.DeepSeekClient;
+import io.github.pigmesh.ai.deepseek.core.chat.ChatCompletionResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+/**
+ * @author: lei
+ * @date: 2025-02-25 15:58
+ * @desc:  https://try.pig4cloud.com/sse.html
+ */
+@RestController
+public class DeepSeekController {
+
+
+
+    @Autowired
+    private DeepSeekClient deepSeekClient;
+
+    @CrossOrigin("*")
+    @GetMapping(value = "/chat", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<ChatCompletionResponse> chat(String prompt) {
+        String a ="";
+        return deepSeekClient.chatFluxCompletion(prompt);
+    }
+
+
+}
---
+++ springboot-deepseek/src/main/java/com/leilei/SpringbootDeepSeekApplication.java
@@ -0,0 +1,17 @@
+package com.leilei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @author: lei
+ * @date: 2025-02-25 15:56
+ * @desc:
+ */
+@SpringBootApplication
+public class SpringbootDeepSeekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDeepSeekApplication.class, args);
+    }
+
+}
